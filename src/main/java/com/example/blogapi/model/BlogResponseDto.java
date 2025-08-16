a11a5ad108bb3cfedcce6e6a37aba696123f83f8
Christian Jay Tibi <christianjaytibi@gmail.com>
2025-08-16 13:22:34 +0800

    rename DTO class names for clarity

--- src/main/java/com/example/blogapi/model/BlogResponseDto.java
+++ src/main/java/com/example/blogapi/model/BlogResponseDto.java
@@ -1,15 +1,21 @@
 package com.example.blogapi.model;
 
+import java.time.Instant;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
-public class BlogCreateDto {
+@Setter
+public class BlogResponseDto {
+    private Long id;
     private String title;
     private String content;
     private String category;
     private Set<String> tags;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
--- src/main/java/com/example/blogapi/model/CreateBlogRequestDTO.java
+++ src/main/java/com/example/blogapi/model/CreateBlogRequestDTO.java
@@ -1,21 +1,15 @@
 package com.example.blogapi.model;
 
-import java.time.Instant;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
-@Setter
-public class BlogDto {
-    private Long id;
+public class CreateBlogRequestDTO {
     private String title;
     private String content;
     private String category;
     private Set<String> tags;
-    private Instant createdAt;
-    private Instant updatedAt;
 }
